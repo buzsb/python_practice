c1f119e1a7aeb648181efc7b3d98f1c6154fd6f8
Buz Oleksandr <oleksandr.buz@gmail.com>
2016-11-11 17:25:33 +0200

    adds factorial function and tests for her

--- src/very_basic_operations.py
+++ src/very_basic_operations.py
@@ -2,5 +2,17 @@ def add(a, b):
     """Basic function which adds two numbers"""
     return a + b 
 
+def factorial(number):
+    n = 1
+    if type(number) != int or number < 0:
+        raise ValueError 
+    else:
+        for i in range(number):
+            n = n * (i+1)
+    return n
+
+
 if __name__ == '__main__':
     print add(2, 3)
+
+    print factorial(4)
--- tests/test_very_basic_operations.py
+++ tests/test_very_basic_operations.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.very_basic_operations import add
+from src.very_basic_operations import add, factorial
 
 
 class AddingTwoNumbersTest(unittest.TestCase):
@@ -9,6 +9,21 @@ class AddingTwoNumbersTest(unittest.TestCase):
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(0, 3), 3)
         self.assertEqual(add(-1, 3), 2)
+
+class FactorialTest(unittest.TestCase):
+
+    def test_equality(self):
+        self.assertEqual(factorial(0), 1)
+        self.assertEqual(factorial(3), 6)
+        self.assertEqual(factorial(5), 120)
+
+    def test_argument_float(self):
+        with self.assertRaises(ValueError):
+            factorial(2.4)
+
+    def test_argument_less_than_zero(self):
+        with self.assertRaises(ValueError):
+            factorial(-2)
 
 if __name__ == '__main__':
     unittest.main()
