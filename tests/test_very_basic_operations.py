a0aadf01abc6fac5ef06b92fe8454a1960a8a8bf
Buz Oleksandr <oleksandr.buz@gmail.com>
2016-11-11 15:50:46 +0200

    corrected tests code

--- tests/test_very_basic_operations.py
+++ tests/test_very_basic_operations.py
@@ -7,11 +7,7 @@ class AddingTwoNumbersTest(unittest.TestCase):
 
     def test_equality(self):
         self.assertEqual(add(2, 3), 5)
-
-    def test_equality(self):
         self.assertEqual(add(0, 3), 3)
-
-    def test_equality(self):
         self.assertEqual(add(-1, 3), 2)
 
 if __name__ == '__main__':
