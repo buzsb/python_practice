620e71609fd3254743896e50b08de8d648cb53e1
Buz Oleksandr <oleksandr.buz@gmail.com>
2016-11-16 12:12:56 +0200

    corrected code

--- src/max_number_occurence.py
+++ src/max_number_occurence.py
@@ -5,19 +5,20 @@ def generated_list():
     random_generated_list = []
     while len(random_generated_list) < 10:
         random_generated_list.append(random.randint(0, 9))
+    print random_generated_list
     return random_generated_list
 
 
-def max_number_occurence():
-    numders_occurensces = {}
+def max_number_occurence(generated_list):
+    numbers_occurensces = {}
 
-    for i in generated_list():
-        if numders_occurensces.has_key(i):
-            numders_occurensces[i] += 1
+    for i in generated_list:
+        if i in  numbers_occurensces:
+            numbers_occurensces[i] += 1
         else:
-            numders_occurensces[i] = 1
-    return numders_occurensces
+            numbers_occurensces[i] = 1
+    return numbers_occurensces
 
 
 if __name__ == '__main__':
-    print max_number_occurence()
+    print max_number_occurence(generated_list())
