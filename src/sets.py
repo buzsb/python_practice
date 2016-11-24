852153f94002fbf33c214a6c60515760392a222b
Buz Oleksandr <oleksandr.buz@gmail.com>
2016-11-24 11:58:38 +0200

    added new function in src/sets file and tests for her in tests/test_sets

--- src/sets.py
+++ src/sets.py
@@ -1,5 +1,5 @@
 def unique_elements_in_list(array):
-    
+
     unique_elements = [i for i in set(array)]
     print unique_elements
     return unique_elements
@@ -7,3 +7,4 @@ def unique_elements_in_list(array):
 
 if __name__ == '__main__':
     unique_elements_in_list([1, 2, 3, 3, 4, 4])
+    
--- tests/test_sets.py
+++ tests/test_sets.py
@@ -1,4 +1,4 @@
-import unittests
+import unittest
 
 from src.sets import unique_elements_in_list
 
@@ -7,11 +7,23 @@ class UniqueElementsInListTest(unittest.TestCase):
 
     def test_equality(self):
         test_list = [1, 1, 2, 3, 3]
-        self.assertEqual(unique_elements_in_list(test_list), [1, 2, 3])
+        self.assertEqual(sorted(unique_elements_in_list(test_list)), [1, 2, 3])
 
         second_test_list = [4, 4, 5, 3, 5]
-        self.assertEqual(unique_elements_in_list(second_test_list), [3, 4, 5])
+        self.assertEqual(sorted(unique_elements_in_list(second_test_list)), [3, 4, 5])
+
+    def test_second_equality(self):
+        test_list = [2, 2, 3, 4, 3]
+
+        def list_check(mait_list):
+            unique_elements_list = unique_elements_in_list(test_list)
+            for i in unique_elements_list:
+                if i in mait_list:
+                    return True
+
+        self.assertTrue(list_check(test_list))
 
 
 if __name__ == '__main__':
     unittest.main()
+    
