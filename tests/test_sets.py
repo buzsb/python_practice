056e5e9bd7a096303a48d469525c79524eae5276
Buz Oleksandr <oleksandr.buz@gmail.com>
2016-11-24 12:45:27 +0200

    corrected test_second_equality function and added new test in tests/test_sets

--- tests/test_sets.py
+++ tests/test_sets.py
@@ -15,13 +15,20 @@ class UniqueElementsInListTest(unittest.TestCase):
     def test_second_equality(self):
         test_list = [2, 2, 3, 4, 3]
 
-        def list_check(mait_list):
+        def list_check(main_list):
             unique_elements_list = unique_elements_in_list(test_list)
             for i in unique_elements_list:
-                if i in mait_list:
-                    return True
+                if i not in main_list:
+                    return False
+            return True
 
         self.assertTrue(list_check(test_list))
+
+    def test_short_true_check(self):
+        test_list = [3, 4, 4, 5]
+        unique_elements_list = unique_elements_in_list(test_list)
+
+        self.assertTrue(all(i in test_list for i in unique_elements_list))
 
 
 if __name__ == '__main__':
