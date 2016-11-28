74acc3c9e63eff163a9cef6eff48e5ef37b2d67b
Buz Oleksandr <oleksandr.buz@gmail.com>
2016-11-28 14:35:57 +0200

    modified generator function

--- src/generators.py
+++ src/generators.py
@@ -1,9 +1,12 @@
-def range_generator(start, end):
+def range_generator(start, end=0):
+    if end == 0:
+        end = start
+        start = 0
     while start < end:
         yield start
         start += 1
 
 
 if __name__ == '__main__':
-    for x in range_generator(0, 5):
+    for x in range_generator(5):
         print x
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -7,7 +7,7 @@ class RangeGeneratorTest(unittest.TestCase):
 
     def test_equality(self):
         test_list = []
-        for i in range_generator(0, 5):
+        for i in range_generator(5):
             test_list.append(i)
         self.assertEqual(test_list, range(5))
 
