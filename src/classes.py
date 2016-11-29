d852d2b53e0e62ed7fc0fab08bf46055168664f8
Buz Oleksandr <oleksandr.buz@gmail.com>
2016-11-29 14:24:10 +0200

    added new method in Square class

--- src/classes.py
+++ src/classes.py
@@ -3,10 +3,16 @@ class Square(object):
     def __init__(self, length):
         self.length = length
 
+    def __repr__(self):
+        return ('Square with length {length} has area {area}'
+                .format(length=self.length, area=self.length ** 2)
+                )
+
     def square_area(self):
-        return self.length * self.length
+        return self.length ** 2
 
 
 if __name__ == '__main__':
     a = Square(5)
     print a.square_area()
+    print a.__repr__()
