126e7b08b80355e02b579ac242ac97266ba83af7
Buz Oleksandr <oleksandr.buz@gmail.com>
2016-12-05 15:38:41 +0200

    added new functions and tests for them

--- src/other_technologies.py
+++ src/other_technologies.py
@@ -8,6 +8,18 @@ def other_cubed_list(array):
     return cubed_list
 
 
+def bigger_than_n(array, n):
+    bigger_list = list(filter(lambda i: i > n, array))
+    return bigger_list
+
+
+def other_bigger_than_n(array, n):
+    bigger_list = [i for i in array if i > n]
+    return bigger_list
+
+
 if __name__ == '__main__':
     print cubed_list([2, 3, 4])
     print other_cubed_list([2, 3, 4])
+    print bigger_than_n([2, 3, 5, 7, 8], 5)
+    print other_bigger_than_n([2, 3, 5, 7, 8], 5)
