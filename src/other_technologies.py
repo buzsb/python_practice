ddad5a1f450e6762a9b6f0d53d20afc942b85b5b
Buz Oleksandr <oleksandr.buz@gmail.com>
2016-12-05 15:16:22 +0200

    added function which use list comprehensions and test for it

--- src/other_technologies.py
+++ src/other_technologies.py
@@ -3,5 +3,11 @@ def cubed_list(array):
     return cubed_list
 
 
+def other_cubed_list(array):
+    cubed_list = [i**3 for i in array]
+    return cubed_list
+
+
 if __name__ == '__main__':
     print cubed_list([2, 3, 4])
+    print other_cubed_list([2, 3, 4])
--- tests/test_other_technologies.py
+++ tests/test_other_technologies.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.other_technologies import cubed_list
+from src.other_technologies import cubed_list, other_cubed_list
 
 
 class TestCubedList(unittest.TestCase):
@@ -8,6 +8,13 @@ class TestCubedList(unittest.TestCase):
     def test_equality(self):
         test_list = [2, 3, 4]
         self.assertEqual(cubed_list(test_list), [8, 27, 64])
+
+
+class TestOtherCubedList(unittest.TestCase):
+
+    def test_equality(self):
+        test_list = [2, 3, 4]
+        self.assertEqual(other_cubed_list(test_list), [8, 27, 64])
 
 
 if __name__ == '__main__':
