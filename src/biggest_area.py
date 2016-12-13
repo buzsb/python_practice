27d065eca37cc62e6b314e2e9ee2bed9166ad339
Buz Oleksandr <oleksandr.buz@gmail.com>
2016-12-13 16:46:48 +0200

    not

--- src/biggest_area.py
+++ src/biggest_area.py
@@ -1,20 +1,20 @@
 def biggest_area(array):
     result = 0
-    for i in array:
-        if i == 0:
+    for i in range(len(array)):
+        if array[i] == 0:
             continue
         length = 1
-        heigth = i
-        position = array.index(i)
+        heigth = array[i]
+        position = i
         while position < len(array) - 1:
-            if array[position + 1] >= i:
+            print position
+            if array[position + 1] >= array[i]:
                 length += 1
             else:
                 break
             position += 1
 
         temporary_result = length * heigth
-        print temporary_result
         if temporary_result > result:
             result = temporary_result
     return result
