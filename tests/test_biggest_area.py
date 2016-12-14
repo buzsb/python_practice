168c039dbfed52a96026a8021aef4e958b2cdd01
Buz Oleksandr <oleksandr.buz@gmail.com>
2016-12-14 16:07:06 +0200

    added new tests

--- tests/test_biggest_area.py
+++ tests/test_biggest_area.py
@@ -12,6 +12,14 @@ class BiggestAreaTest(unittest.TestCase):
         test_array = [4, 2, 3, 4]
         self.assertEqual(biggest_area(test_array), 8)
 
+    def test_equality_with_zero(self):
+        test_array = [4, 0, 3, 4]
+        self.assertEqual(biggest_area(test_array), 6)
+
+    def test_equality_one_big_number(self):
+        test_array = [4, 5, 0, 12]
+        self.assertEqual(biggest_area(test_array), 12)
+
 
 if __name__ == '__main__':
     unittest.main()
