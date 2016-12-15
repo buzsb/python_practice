758ce073dc1dd0c9d22991394822e4fad6c2dfdb
Buz Oleksandr <oleksandr.buz@gmail.com>
2016-12-15 17:16:06 +0200

    add few corrections in fifteenki function

--- src/fifteenki.py
+++ src/fifteenki.py
@@ -9,14 +9,13 @@ def generated_list(number):
 
 def fifteenki(height=4, length=4):
     array = generated_list(height * length)
-    if 0 in array:
-        array[array.index(0)] = ''
+    array[array.index(0)] = ''
     matrix = []
     for i in xrange(0, len(array), length):
         matrix.append(array[i:i + length])
     for row in matrix:
-        print (''.join(['{:>3}'.format(item) for item in row]))
+        print ''.join(['{:>3}'.format(item) for item in row])
 
 
 if __name__ == '__main__':
-    fifteenki()
+    fifteenki(7, 7)
