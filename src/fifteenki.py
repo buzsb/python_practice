340f57b167adedef25d6b309987fa1f50b70358e
Buz Oleksandr <oleksandr.buz@gmail.com>
2016-12-15 17:04:55 +0200

    corrected code

--- src/fifteenki.py
+++ src/fifteenki.py
@@ -15,7 +15,7 @@ def fifteenki(height=4, length=4):
     for i in xrange(0, len(array), length):
         matrix.append(array[i:i + length])
     for row in matrix:
-        print ('\n'.join([''.join(['{:>3}'.format(item) for item in row])]))
+        print (''.join(['{:>3}'.format(item) for item in row]))
 
 
 if __name__ == '__main__':
