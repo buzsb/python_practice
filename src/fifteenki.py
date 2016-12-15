09017697804268f213ffad6229e16f9483ca0038
Buz Oleksandr <oleksandr.buz@gmail.com>
2016-12-15 15:57:12 +0200

    modified fifteenki code but not for end

--- src/fifteenki.py
+++ src/fifteenki.py
@@ -7,16 +7,16 @@ def generated_list(number):
     return array
 
 
-def fifteenki():
-    generated_lst = generated_list(16)
-    if 0 in generated_lst:
-        generated_lst[generated_lst.index(0)] = ''
-    print(
-        '{lst[0]:<3}{lst[1]:<3}{lst[2]:<3}{lst[3]:<3}\n'
-        '{lst[4]:<3}{lst[5]:<3}{lst[6]:<3}{lst[7]:<3}\n'
-        '{lst[8]:<3}{lst[9]:<3}{lst[10]:<3}{lst[11]:<3}\n'
-        '{lst[12]:<3}{lst[13]:<3}{lst[14]:<3}{lst[15]:<3}'
-    ).format(lst=generated_lst)
+def fifteenki(height=4, length=4):
+    generated_lst = generated_list(height * length)
+    matrix = []
+    for i in xrange(0, len(generated_lst), length):
+        matrix.append(generated_lst[i:i + length])
+    print matrix
+    for row in matrix:
+        for colunm in row:
+            print ('{:>3}').format(colunm)
+        print
 
 
 if __name__ == '__main__':
