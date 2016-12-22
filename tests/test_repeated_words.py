195d0b4212f0ee5325b4c51cf4a6cf5bd4ed4820
Buz Oleksandr <oleksandr.buz@gmail.com>
2016-12-22 14:08:04 +0200

    corrected code in function and in tests

--- tests/test_repeated_words.py
+++ tests/test_repeated_words.py
@@ -7,15 +7,18 @@ class TestRepeatWords(unittest.TestCase):
 
     def test_equality(self):
         test_list = ['cat', 'Cat', 'doG', 'dog', 'COW']
-        self.assertEqual(len(repeated_words(test_list)), 2)
+        repeated = set(['cat', 'dog'])
+        self.assertEqual(repeated_words(test_list), repeated)
 
     def test_second_equality(self):
         test_list = ['cat', 'doG', 'COW']
-        self.assertEqual(len(repeated_words(test_list)), 0)
+        repeated = set()
+        self.assertEqual(repeated_words(test_list), repeated)
 
     def test_empty_list(self):
         test_list = []
-        self.assertEqual(len(repeated_words(test_list)), 0)
+        repeated = set()
+        self.assertEqual(repeated_words(test_list), repeated)
 
 
 if __name__ == '__main__':
