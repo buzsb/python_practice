36b4d745ef6e5ef806b56f340e22c1d42e52bfc4
Buz Oleksandr <oleksandr.buz@gmail.com>
2016-12-22 14:08:04 +0200

    corrected code in function and in tests

--- src/repeated_words.py
+++ src/repeated_words.py
@@ -1,12 +1,14 @@
 def repeated_words(words_array):
     lower_case_words_array = [i.lower() for i in words_array]
+    repeated = set()
     result = set()
     for i in lower_case_words_array:
-        if lower_case_words_array.count(i) > 1:
+        if i in repeated:
             result.add(i)
-    print result
+        else:
+            repeated.add(i)
     return result
 
 
 if __name__ == '__main__':
-    repeated_words(['cat', 'Cat', 'doG', 'dog', 'COW'])
+    print repeated_words(['cat', 'Cat', 'doG', 'dog', 'COW'])
