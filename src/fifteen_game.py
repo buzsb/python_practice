92bb17d8fad28449048d560676caf6a1808ca23f
Buz Oleksandr <oleksandr.buz@gmail.com>
2016-12-23 14:09:21 +0200

    add new file fifteen_game

--- src/fifteen_game.py
+++ src/fifteen_game.py
@@ -1,27 +1,27 @@
 import random
 
 
-class FifteenkiGame(object):
+class FifteenGame(object):
 
     def __init__(self, height=4, length=4):
         self.height = height
         self.length = length
+        self.field = self.generated_list(self.height * self.length)
 
     def generated_list(self, number):
         array = range(number)
         random.shuffle(array)
         return array
 
-    def fifteenki(self):
-        array = self.generated_list(self.height * self.length)
-        array[array.index(0)] = ''
+    def print_field(self):
+        self.field[self.field.index(0)] = ''
         matrix = []
-        for i in xrange(0, len(array), self.length):
-            matrix.append(array[i:i + self.length])
+        for i in xrange(0, len(self.field), self.length):
+            matrix.append(self.field[i:i + self.length])
         for row in matrix:
             print ''.join(['{:>3}'.format(item) for item in row])
 
 
 if __name__ == '__main__':
-    a = FifteenkiGame()
-    a.fifteenki()
+    a = FifteenGame()
+    a.print_field()
