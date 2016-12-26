4dd512795b9efdd16d4ad9ec384532be1e2afffd
Buz Oleksandr <oleksandr.buz@gmail.com>
2016-12-26 13:46:22 +0200

    this shit is no working anymore

--- src/fifteen_game.py
+++ src/fifteen_game.py
@@ -7,8 +7,9 @@ class FifteenGame(object):
         self.height = height
         self.length = length
         self.field = self.generated_list(self.height * self.length)
-        self.x = 0
-        self.y = 0
+        self.empty_coordinates()
+        self.empty_x = 0
+        self.empty_y = 0
 
     def generated_list(self, number):
         array = range(number)
@@ -23,24 +24,25 @@ class FifteenGame(object):
         for row in self.field:
             print ''.join(['{:>3}'.format(item) for item in row])
 
-    def coordinates(self):
+    def empty_coordinates(self):
         for i in self.field:
             if '' in i:
-                self.x = self.field.index(i)
-                self.y = i.index('')
+                self.empty_y = self.field.index(i)
+                self.empty_x = i.index('')
 
     def move_up(self):
-        if self.x == 0:
+        if self.empty_y == 0:
             return False
         print ''
-        self.field[self.x][self.y], self.field[self.x - 1][self.y] = (
-            self.field[self.x - 1][self.y], self.field[self.x][self.y])
+        print self.empty_x, self.empty_y
+        self.field[self.empty_x][self.empty_y], self.field[self.empty_x][self.empty_y - 1] = (
+            self.field[self.empty_x][self.empty_y - 1], self.field[self.empty_x][self.empty_y])
 
-        self.x = self.x - 1
+        self.empty_y = self.empty_y - 1
         self.print_field()
 
     def move_down(self):
-        if self.x == len(self.field) - 1:
+        if self.empty_y == len(self.field) - 1:
             return False
         print ''
         self.field[self.x][self.y], self.field[self.x + 1][self.y] = (
@@ -50,7 +52,7 @@ class FifteenGame(object):
         self.print_field()
 
     def move_left(self):
-        if self.y == 0:
+        if self.empty_x == 0:
             return False
         print ''
         self.field[self.x][self.y], self.field[self.x][self.y - 1] = (
@@ -60,7 +62,7 @@ class FifteenGame(object):
         self.print_field()
 
     def move_right(self):
-        if self.y == len(self.field[0]) - 1:
+        if self.empty_x == len(self.field[0]) - 1:
             return False
         print ''
         self.field[self.x][self.y], self.field[self.x][self.y + 1] = (
@@ -72,6 +74,6 @@ class FifteenGame(object):
 
 if __name__ == '__main__':
     a = FifteenGame()
-    a.coordinates()
+    a.empty_coordinates()
     a.print_field()
-    a.move_right()
+    a.move_up()
