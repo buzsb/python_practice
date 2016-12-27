746234021bb8bbdb1c2f8cda2b49c2fac8c8e753
Buz Oleksandr <oleksandr.buz@gmail.com>
2016-12-27 14:41:11 +0200

    added win check function

--- src/fifteen_game.py
+++ src/fifteen_game.py
@@ -3,10 +3,10 @@ import random
 
 class FifteenGame(object):
 
-    def __init__(self, height=4, length=4):
+    def __init__(self, height, width):
         self.height = height
-        self.length = length
-        self.field = self.generated_list(self.height * self.length)
+        self.width = width
+        self.field = self.generated_list(self.height * self.width)
         self.empty_coordinates()
 
     def generated_list(self, number):
@@ -14,8 +14,8 @@ class FifteenGame(object):
         random.shuffle(array)
         array[array.index(0)] = ''
         field = []
-        for i in xrange(0, len(array), self.length):
-            field.append(array[i:i + self.length])
+        for i in xrange(0, len(array), self.width):
+            field.append(array[i:i + self.width])
         return field
 
     def print_field(self):
@@ -57,16 +57,30 @@ class FifteenGame(object):
         self.print_field()
 
     def move_right(self):
-        if self.empty_x == self.length - 1:
+        if self.empty_x == self.width - 1:
             return False
         self.swap(self.empty_y, self.empty_x + 1, self.empty_y, self.empty_x)
 
         self.empty_x = self.empty_x + 1
         self.print_field()
 
+    def win_check(self):
+        check = False
+        for i in self.field:
+            for x, number in enumerate(i):
+                y = self.field.index(i)
+                if number == '':
+                    continue
+                if number == y * self.width + x + 1:
+                    check = True
+                else:
+                    check = False
+                print number, y * self.width + x + 1, check
+        return check
 
-def play_game():
-    a = FifteenGame()
+
+def play_game(height=4, width=4):
+    a = FifteenGame(height, width)
     a.print_field()
     print (
         'Use w - to move up,\n'
@@ -79,15 +93,23 @@ def play_game():
         move = raw_input()
         if move == 'w':
             a.move_up()
+            if a.win_check():
+                break
         if move == 's':
             a.move_down()
+            if a.win_check():
+                break
         if move == 'a':
             a.move_left()
+            if a.win_check():
+                break
         if move == 'd':
             a.move_right()
+            if a.win_check():
+                break
         if move == 'e':
             break
 
 
 if __name__ == '__main__':
-    play_game()
+    play_game(2, 2)
