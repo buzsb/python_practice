92f901759e6b2e0977384c917741c72bd572ecb3
Bogdan Buz <bogdan@tubularlabs.com>
2017-01-04 13:06:02 +0200

    added docstring for conversion_string_to_integer function

--- src/str_to_int_conversion.py
+++ src/str_to_int_conversion.py
@@ -1,4 +1,15 @@
 def conversion_string_to_integer(string_number):
+    """Converts string to integer.
+
+    Args:
+        string_number (str): String that would be converted to integer.
+
+    Returns:
+        int: Converted number.
+
+    Raises:
+        ValueError: Raised when string contains characters that are not digits.
+    """
     result = 0
     for i, digit in enumerate(string_number):
         if ord(digit) < ord('0') or ord(digit) > ord('9'):
