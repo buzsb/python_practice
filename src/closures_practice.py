ae1d2e147cbdda5434a02a49420d722d1ff126d5
Buz Oleksandr <oleksandr.buz@gmail.com>
2017-01-11 11:13:57 +0200

    change inner work number_in_range_checker function, added docstring for make_check_if_number_in_range

--- src/closures_practice.py
+++ src/closures_practice.py
@@ -1,11 +1,18 @@
 def make_check_if_number_in_range(range_start, range_end):
+    """Check if number in some range.
+
+    Args:
+        range_start (int or float): Beginning of range.
+        range_end (int of float): End of range.
+
+    Returns:
+        boolean: True or False
+    """
     def number_in_range_checker(number):
-        if number < range_start or number > range_end:
-            return False
-        return True
+        return range_start < number < range_end
     return number_in_range_checker
 
 
 if __name__ == '__main__':
     checkted_number = make_check_if_number_in_range(1.5, 10)
-    print checkted_number(8)
+    print checkted_number(18)
