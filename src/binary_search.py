49bdead3e8b5d8d310ca4b5822ae6d22b14584b0
Buz Oleksandr <oleksandr.buz@gmail.com>
2017-04-03 15:49:17 +0300

    added recursive_binary_search function in binary_search file

--- src/binary_search.py
+++ src/binary_search.py
@@ -15,6 +15,26 @@ def binary_search(sorted_list, searched_element):
     return None
 
 
+def recursive_binary_search(sorted_list, searched_element, start, end):
+    if sorted_list == []:
+        return None
+    if start > end:
+        return None
+    middle_element = (start + end) / 2
+    if sorted_list[middle_element] == searched_element:
+            return middle_element
+
+    if sorted_list[middle_element] < searched_element:
+        return recursive_binary_search(
+            sorted_list, searched_element, middle_element + 1, end
+        )
+    else:
+        return recursive_binary_search(
+            sorted_list, searched_element, start, middle_element - 1
+        )
+
+
 if __name__ == '__main__':
     array = [1, 2, 3, 5]
     print binary_search(array, 5)
+    print recursive_binary_search(array, 6, 0, len(array) - 1)
