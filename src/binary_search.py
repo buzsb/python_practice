5f7f4c78f8602deacb98488ced3c38e179a0fb48
Buz Oleksandr <oleksandr.buz@gmail.com>
2017-04-05 15:01:03 +0300

    corrected another_recursive_binary_search function, added tests for another_recursive_binary_search function

--- src/binary_search.py
+++ src/binary_search.py
@@ -39,16 +39,21 @@ def recursive_binary_search(sorted_list, searched_element, start=0, end=None):
 def another_recursive_binary_search(sorted_list, searched_element):
     if sorted_list == []:
         return None
+    if len(sorted_list) == 1 and sorted_list[0] is not searched_element:
+        return None
     start = 0
-    end = len(sorted_list) - 1
+    end = len(sorted_list)
     middle_element = (start + end) / 2
     if sorted_list[middle_element] == searched_element:
         return middle_element
 
     if sorted_list[middle_element] < searched_element:
-        return another_recursive_binary_search(
+        position_in_right_part = another_recursive_binary_search(
             sorted_list[middle_element:], searched_element
         )
+        if position_in_right_part is None:
+            return None
+        return position_in_right_part + middle_element
     else:
         return another_recursive_binary_search(
             sorted_list[:middle_element], searched_element
@@ -59,4 +64,4 @@ if __name__ == '__main__':
     array = [1, 2, 3, 5]
     print binary_search(array, 5)
     print recursive_binary_search(array, 1)
-    print another_recursive_binary_search(array, 3)
+    print another_recursive_binary_search(array, 5)
--- tests/test_binary_search.py
+++ tests/test_binary_search.py
@@ -1,6 +1,6 @@
 import unittest
 from src.binary_search import binary_search, \
-    recursive_binary_search
+    recursive_binary_search, another_recursive_binary_search
 
 
 class BinarySearchTest(unittest.TestCase):
@@ -53,6 +53,37 @@ class RecursiveBinarySearchTest(unittest.TestCase):
     def test_empty_list(self):
         test_list = []
         self.assertIsNone(recursive_binary_search(test_list, 8))
+
+
+class AnotherRecursiveBinarySearchTest(unittest.TestCase):
+
+    def test_element_in_list_recursive(self):
+        test_list = [1, 2, 3, 4]
+        searched_element_index = test_list.index(3)
+        self.assertEqual(
+            another_recursive_binary_search(test_list, 3),
+            searched_element_index
+        )
+
+    def test_element_not_in_list(self):
+        test_list = [1, 2, 3, 4]
+        self.assertIsNone(another_recursive_binary_search(test_list, 8))
+
+    def test_list_with_one_searched_element(self):
+        test_list = [3]
+        searched_element_index = test_list.index(3)
+        self.assertEqual(
+            another_recursive_binary_search(test_list, 3),
+            searched_element_index
+        )
+
+    def test_list_with_one_element(self):
+        test_list = [3]
+        self.assertIsNone(another_recursive_binary_search(test_list, 8))
+
+    def test_empty_list(self):
+        test_list = []
+        self.assertIsNone(another_recursive_binary_search(test_list, 8))
 
 
 if __name__ == '__main__':
