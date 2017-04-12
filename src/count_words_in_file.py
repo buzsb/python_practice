fd074ad3c7865feb6d6a2bc7a1fdf727740dc4de
Buz Oleksandr <oleksandr.buz@gmail.com>
2017-04-12 13:33:18 +0300

    change file_lines_to_words_list_converter function

--- src/count_words_in_file.py
+++ src/count_words_in_file.py
@@ -1,15 +1,13 @@
 def file_lines_to_words_list_converter():
     file = open("src/text_file.txt")
     words_list = []
-    for line in file:
-        line_list = line.split()
-        for word in line_list:
-            word = list(word)
-            while ord(word[-1]) < ord('A') or ord(word[-1]) > ord('z'):
-                word = word[:-1]
-                if len(word) == 0:
-                    break
-            words_list.append(''.join(word))
+    text = ''.join(file.readlines())
+    start_word = 0
+    for i, digit in enumerate(text):
+        if ord(digit) < ord('A') or ord(digit) > ord('z'):
+            word = text[start_word:i]
+            words_list.append(word)
+            start_word = i + 1
     return words_list
 
 
