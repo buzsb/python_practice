3d32e8c15d322069ff953db7523aa7bf71031c4f
Buz Oleksandr <oleksandr.buz@gmail.com>
2017-04-13 13:27:53 +0300

    created file with passwords validity checker program

---
+++ src/passwords_validity_checker.py
@@ -0,0 +1,24 @@
+import re
+
+
+def password_validity_checker(passwords_list):
+    good_passwords_list = []
+    passwords = passwords_list.split()
+    for i, password in enumerate(passwords):
+        if len(password) >= 6 and len(password) <= 12:
+            if not re.search(r'[A-Z]', password):
+                continue
+            elif not re.search(r'[a-z]', password):
+                continue
+            elif not re.search(r'[0-9]', password):
+                continue
+            elif not re.search(r'[$#@]', password):
+                continue
+            else:
+                good_passwords_list.append(passwords[i])
+    return good_passwords_list
+
+
+if __name__ == '__main__':
+    passwords = 'password, 1111, P23Una#jd, GoodPass1$'
+    print password_validity_checker(passwords)
